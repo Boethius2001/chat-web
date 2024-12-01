3c21d0bcb8256dc5091bec5d3f75ff1fc43111d5
Boethius <emrecan.senel2001@gmail.com>
2024-12-01 14:27:38 +0300

    basic-chat

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const path = require('path');
+const chat_data = require('./src/data.json');
 
 const app = express();
 const PORT = 3000;
 
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname,'views')));
+
 app.get('/', (req, res)=>{
-    res.send('<h1>working!!</h1>');
+    res.render('menu');
+});
+
+app.get('/chat', (req, res)=>{
+    res.render(`chat`, {chat_data});
+});
+
+app.post('/chat', (req, res)=>{
+    const new_data = req.body;
+    chat_data.push(new_data);
+    res.redirect('/chat');
 });
 
 app.listen(PORT, ()=>{
